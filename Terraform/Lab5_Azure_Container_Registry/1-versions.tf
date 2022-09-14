a690a21f0e103d1b085c7dcc4feb1ad1dd9c962d
erwanlancien <erwan.lancien@gmail.com>
2022-09-14 11:29:18 +0200

    fix 2

--- Terraform/Lab5_Azure_Container_Registry/1-versions.tf
+++ Terraform/Lab5_Azure_Container_Registry/1-versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 1.1.7"
   # cf. https://www.terraform.io/language/settings/backends/azurerm
   backend "azurerm" {
-    resource_group_name  = "RG-AKSCluster"         # mettre ici le nom du resource group de vos ressource
+    resource_group_name  = "RG-TalanTFLab"         # mettre ici le nom du resource group de vos ressource
     storage_account_name = "terraformeln"      # mettre le nom du compte de stockage créer dans le lab 1
     container_name       = "tfstate"
     key                  = "acr.terraform.tfstate" 
